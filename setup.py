10b48853e29af3f2b37f59fae5b7b7b023be13c9
LucaDeGrandis <luca.degrandis@altiliagroup.com>
2025-06-26 11:30:57 +0200

    [FIX] updated setup.py to install the requirements automatically

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="form_visual_source_attribution",  # Replace with your module/package name
     version="0.1.0",
@@ -8,4 +11,5 @@ setup(
     description="Aligns multimodal embeddings with text and image inputs for visual source attribution.",
     url="https://github.com/DarumaAI/form_visual_source_attribution.git",  # Optional
     python_requires=">=3.10",  # Adjust as needed
+    install_requires=requirements,
 )
